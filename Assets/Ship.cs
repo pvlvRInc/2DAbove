eeab91a883a047777bf0b37364bc969176e4ef36
pvlvRInc <pra_dev@mail.ru>
2023-04-10 17:05:11 +0500

    rename controller + create input interface

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Ship : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
-    [SerializeField] private float forceMulti;
-    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float _forceMulti;
+    [SerializeField] private float _rotationSpeed;
 
     private void Awake()
     {
@@ -19,17 +19,17 @@ public class PlayerController : MonoBehaviour
         Vector3 dirToMouse = (mousePos - transform.position);
         float dirAngle = Mathf.Atan2(dirToMouse.normalized.x, dirToMouse.normalized.y) * Mathf.Rad2Deg;
 
-        mousePos.z = transform.position.z;
+        float rotationSpeed = 0.01f * _rotationSpeed * dirToMouse.magnitude / 3;
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.back * dirAngle), 0.01f * rotationSpeed * dirToMouse.magnitude / 3);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.back * dirAngle), rotationSpeed);
 
 
         if (Input.GetMouseButton(0))
         {
-            _rigidbody.AddForce(transform.up * Time.deltaTime * forceMulti * Mathf.Min(Mathf.Max(dirToMouse.magnitude - 10.05f, 0), 1));
+            _rigidbody.AddForce(transform.up * Time.deltaTime * _forceMulti * Mathf.Min(Mathf.Max(dirToMouse.magnitude - 10.05f, 0), 1));
         }
 
-        Debug.Log($"magnitude {dirToMouse.magnitude}");
+        //Debug.Log($"magnitude {dirToMouse.magnitude}");
 
     }
 }
---
+++ IShipInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assembly_CSharp
+{
+    internal interface IShipInput
+    {
+        public Vector3 MouseInput
+        {
+            get; set;
+        }
+
+        public bool MouseDown
+        {
+            get; set;
+        }
+
+        public float InputAngle
+        {
+            get; set;
+        }
+
+    }
+}
