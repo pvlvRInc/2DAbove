952773db50d113fe933c7297ae7176bb9db27dc4
pvlvRInc <pra_dev@mail.ru>
2023-04-12 15:25:07 +0500

    Сhange spawn location pick

--- Assets/Enemy/EnemyConstruct.cs
+++ Assets/Enemy/EnemyConstruct.cs
@@ -9,11 +9,12 @@ public class EnemyConstruct : MonoBehaviour
     [SerializeField] private List<EnemyParameters> _enemyCatalog;
     [SerializeField] private Transform _player;
     [SerializeField] private float _cooldown = 5f;
-
+     
     //Cashing
     private float _startTime;
     private float _points;
     private float _pointIncrement;
+    private Rect _spawnBox;
 
     private float _cooldownEnd;
 
@@ -21,6 +22,11 @@ public class EnemyConstruct : MonoBehaviour
     {
         _startTime = Time.time;
         _cooldownEnd = Time.time + _cooldown;
+
+        _spawnBox.xMin = -20;
+        _spawnBox.xMax = 20;
+        _spawnBox.yMin = -10;
+        _spawnBox.yMax = 10;
     }
 
     public void Update()
@@ -40,9 +46,20 @@ public class EnemyConstruct : MonoBehaviour
             {
                 _points -= _enemyCatalog[pickedEnemyInd].cost;
 
-                GameObject enemy = GameObject.Instantiate(_enemyCatalog[pickedEnemyInd].shape, Vector3.zero, Quaternion.identity);
-                enemy.AddComponent<Enemy>().Initialize(_enemyCatalog[pickedEnemyInd], _player);
+                Vector2 enemySpawnPoint;
+                float border = 5;
+                do
+                {
+                    var x = Random.Range(-_spawnBox.width / 2 - border, _spawnBox.width / 2 + border);
+                    var y = Random.Range(-_spawnBox.height / 2 - border, _spawnBox.height / 2 + border);
+                    enemySpawnPoint = new Vector2(x, y);
+                }
+                while (_spawnBox.Contains(enemySpawnPoint, true));
+                Vector3 spawnPoint = new Vector3(enemySpawnPoint.x, enemySpawnPoint.y, 0);
 
+
+                GameObject enemy = GameObject.Instantiate(_enemyCatalog[pickedEnemyInd].shape, spawnPoint, Quaternion.identity);
+                enemy.AddComponent<Enemy>().Initialize(_enemyCatalog[pickedEnemyInd], _player);
             }
         }
     }
